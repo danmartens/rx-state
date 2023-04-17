4ccb5af3f2565ac3bdf4968d41a85b48f28067b3
Dan Martens <dan@friendsoftheweb.com>
2023-04-16 20:44:10 -0400

    Add publish workflow

---
+++ rollup.config.ts
@@ -0,0 +1,21 @@
+import typescript from '@rollup/plugin-typescript';
+import { defineConfig } from 'rollup';
+
+export default defineConfig({
+  input: ['src/index.ts'],
+  output: {
+    dir: 'dist',
+    format: 'commonjs',
+    preserveModules: true,
+    exports: 'auto',
+    sourcemap: true,
+  },
+  external: ['react', 'react/jsx-runtime', 'rxjs'],
+  plugins: [
+    typescript({
+      jsx: 'react-jsx',
+      emitDeclarationOnly: true,
+      exclude: ['src/**/*.stories.tsx', 'types', 'rollup.config.ts'],
+    }),
+  ],
+});
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export { createStore } from './createStore';
+export { createStoreContext } from './createStoreContext';
+export { ofType } from './ofType';
+export { useStore } from './useStore';
+export { useStoreDispatch } from './useStoreDispatch';
+export { useStoreFactory } from './useStoreFactory';
+export { useStoreSelector } from './useStoreSelector';
+export { useStoreState } from './useStoreState';
+
+export { initializeEffect } from './support/initializeEffect';
+
+export type { Store, Effect, Action } from './types';
