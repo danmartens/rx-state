62d618315dc9f49e631fa8ceddbe13287baf6d54
Dan Martens <dan@friendsoftheweb.com>
2023-04-19 11:51:12 -0400

    Add test helper for dispatching immediately

--- src/support/initializeEffect.ts
+++ src/support/initializeEffect.ts
@@ -14,12 +14,36 @@ export const initializeEffect = <
   const input$ = new Subject<TAction>();
   const output$ = effect(input$, state$, dependencies);
 
+  /**
+   * Dispatches an action during the next event loop.
+   * @example
+   * const {
+   *   dispatch,
+   *   nextAction
+   * } = initializeEffect(effect, state$, dependencies);
+   *
+   * dispatch(inputAction);
+   *
+   * const outputAction = await nextAction();
+   */
   const dispatch = (action: TAction) => {
     queueMicrotask(() => {
       input$.next(action);
     });
   };
 
+  /**
+   * Dispatches an action during the current event loop. This should only be
+   * used to test input actions that don't have a corresponding output action.
+   */
+  const dispatchImmediately = (action: TAction) => {
+    input$.next(action);
+  };
+
+  /**
+   * Returns a `Promise` that resolves with the next action dispatched by the
+   * `Effect`. Accepts an optional `predicate` to filter for the desired action.
+   */
   const nextAction = (
     predicate: (action: TAction) => boolean = () => true
   ): Promise<TAction> => {
@@ -41,6 +65,10 @@ export const initializeEffect = <
     });
   };
 
+  /**
+   * Returns a `Promise` that resolves with the next action of type `actionType`
+   * dispatched by the `Effect`.
+   */
   const nextActionOfType = <T extends Action['type']>(
     actionType: T
   ): Promise<ActionOfType<T>> => {
@@ -62,6 +90,7 @@ export const initializeEffect = <
 
   return {
     dispatch,
+    dispatchImmediately,
     nextAction,
     nextActionOfType,
   };
