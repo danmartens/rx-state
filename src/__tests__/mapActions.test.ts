72ccf6929263c31204f79dd37c9e86bdd24a0b28
Dan Martens <dan@friendsoftheweb.com>
2023-06-28 20:26:56 -0400

    Add operator for mapping actions

---
+++ src/__tests__/mapActions.test.ts
@@ -0,0 +1,41 @@
+import { Subject } from 'rxjs';
+import { mapActions } from '../mapActions';
+
+interface PingAction {
+  type: 'PING';
+}
+
+interface PongAction {
+  type: 'PONG';
+}
+
+type Action = PingAction | PongAction;
+
+describe('mapActions', () => {
+  test('maps one action to another', () => {
+    const action$ = new Subject<Action>();
+    const results: Action[] = [];
+
+    action$
+      .pipe(
+        mapActions({
+          PING: () => ({
+            type: 'PONG',
+          }),
+        })
+      )
+      .subscribe((action) => {
+        results.push(action);
+      });
+
+    action$.next({
+      type: 'PING',
+    });
+
+    expect(results).toEqual([
+      {
+        type: 'PONG',
+      },
+    ]);
+  });
+});
---
+++ src/mapActions.ts
@@ -0,0 +1,25 @@
+import { EMPTY, Observable, OperatorFunction, map, mergeMap, of } from 'rxjs';
+
+import { Action, ActionOfType } from './types';
+
+export const mapActions =
+  <
+    TAction extends Action,
+    TMap extends {
+      [K in TAction['type']]?: (action: ActionOfType<TAction, K>) => TAction;
+    }
+  >(
+    actionMap: TMap
+  ): OperatorFunction<TAction, TAction> =>
+  (action$: Observable<TAction>) =>
+    action$.pipe(
+      mergeMap((action) => {
+        const mapFn = actionMap[action.type as TAction['type']];
+
+        if (mapFn != null) {
+          return of(mapFn(action));
+        }
+
+        return EMPTY;
+      })
+    );
