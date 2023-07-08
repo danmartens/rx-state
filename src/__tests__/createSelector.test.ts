f397a855dfb38a58699f1a38ec9e358d2f6d221b
Dan Martens <dan@friendsoftheweb.com>
2023-07-08 12:42:55 -0400

    Improve create selector (#8)
    * Add input memoization and docs to createSelector
    
    * Add performance measurement to createSelector
    
    * Add compose helper
    
    * Increase test coverage for createSelector
    
    * Improve documentation

--- src/__tests__/createSelector.test.ts
+++ src/__tests__/createSelector.test.ts
@@ -1,9 +1,17 @@
 import { createSelector } from '../createSelector';
 
 type State = Readonly<{
+  users: Readonly<{
+    [id: string]: Readonly<{
+      id: string;
+      name: string;
+    }>;
+  }>;
+
   posts: Readonly<{
     [id: string]: Readonly<{
       id: string;
+      authorId: string;
       title: string;
     }>;
   }>;
@@ -11,6 +19,7 @@ type State = Readonly<{
   comments: Readonly<{
     [id: string]: Readonly<{
       id: string;
+      authorId: string;
       postId: string;
       content: string;
     }>;
@@ -20,8 +29,9 @@ type State = Readonly<{
 describe('createSelector', () => {
   test('with no input selectors', () => {
     const state: State = {
+      users: {},
       posts: {
-        1: { id: '1', title: 'First Post' },
+        1: { id: '1', authorId: '1', title: 'First Post' },
       },
       comments: {},
     };
@@ -36,10 +46,11 @@ describe('createSelector', () => {
 
   test('with one input selector', () => {
     const state: State = {
+      users: {},
       posts: {
-        2: { id: '2', title: 'Second Post' },
-        3: { id: '3', title: 'Third Post' },
-        1: { id: '1', title: 'First Post' },
+        2: { id: '2', authorId: '1', title: 'Second Post' },
+        3: { id: '3', authorId: '1', title: 'Third Post' },
+        1: { id: '1', authorId: '1', title: 'First Post' },
       },
       comments: {},
     };
@@ -63,20 +74,21 @@ describe('createSelector', () => {
 
   test('with two input selectors', () => {
     const state: State = {
+      users: {},
       posts: {
-        1: { id: '1', title: 'First Post' },
-        2: { id: '2', title: 'Second Post' },
-        3: { id: '3', title: 'Third Post' },
+        1: { id: '1', authorId: '1', title: 'First Post' },
+        2: { id: '2', authorId: '1', title: 'Second Post' },
+        3: { id: '3', authorId: '1', title: 'Third Post' },
       },
       comments: {
-        1: { id: '1', postId: '3', content: 'First comment' },
-        2: { id: '2', postId: '1', content: 'Second comment' },
-        3: { id: '3', postId: '1', content: 'Third comment' },
+        1: { id: '1', authorId: '1', postId: '3', content: 'First comment' },
+        2: { id: '2', authorId: '1', postId: '1', content: 'Second comment' },
+        3: { id: '3', authorId: '1', postId: '1', content: 'Third comment' },
       },
     };
 
-    const getPosts = (state: State) => state.posts;
-    const getComments = (state: State) => state.comments;
+    const getPosts = jest.fn((state: State) => state.posts);
+    const getComments = jest.fn((state: State) => state.comments);
 
     const getPostsAndComments = createSelector(
       getPosts,
@@ -106,5 +118,104 @@ describe('createSelector', () => {
     ]);
 
     expect(getPostsAndComments(state)).toBe(getPostsAndComments(state));
+
+    expect(getPosts).toHaveBeenCalledTimes(1);
+    expect(getComments).toHaveBeenCalledTimes(1);
+
+    const nextState = { ...state };
+
+    expect(getPostsAndComments(nextState)).toBe(getPostsAndComments(nextState));
+
+    expect(getPosts).toHaveBeenCalledTimes(2);
+    expect(getComments).toHaveBeenCalledTimes(2);
+  });
+
+  test('with three input selectors', () => {
+    const state: State = {
+      users: {
+        1: { id: '1', name: 'Jane Doe' },
+      },
+      posts: {
+        1: { id: '1', authorId: '1', title: 'First Post' },
+        2: { id: '2', authorId: '1', title: 'Second Post' },
+        3: { id: '3', authorId: '1', title: 'Third Post' },
+      },
+      comments: {
+        1: { id: '1', authorId: '1', postId: '3', content: 'First comment' },
+        2: { id: '2', authorId: '1', postId: '1', content: 'Second comment' },
+        3: { id: '3', authorId: '1', postId: '1', content: 'Third comment' },
+      },
+    };
+
+    const getUsers = jest.fn((state: State) => state.users);
+    const getPosts = jest.fn((state: State) => state.posts);
+    const getComments = jest.fn((state: State) => state.comments);
+
+    const getPostsAndCommentsWithUsers = createSelector(
+      getUsers,
+      getPosts,
+      getComments,
+      (users, posts, comments) =>
+        Object.values(posts).map((post) => ({
+          ...post,
+          author: users[post.authorId],
+          comments: Object.values(comments)
+            .filter((comment) => comment.postId === post.id)
+            .map((comment) => ({
+              ...comment,
+              author: users[comment.authorId],
+            })),
+        }))
+    );
+
+    expect(getPostsAndCommentsWithUsers(state)).toEqual([
+      {
+        ...state.posts['1'],
+        author: state.users['1'],
+        comments: [
+          {
+            ...state.comments['2'],
+            author: state.users['1'],
+          },
+          {
+            ...state.comments['3'],
+            author: state.users['1'],
+          },
+        ],
+      },
+      {
+        ...state.posts['2'],
+        author: state.users['1'],
+        comments: [],
+      },
+      {
+        ...state.posts['3'],
+        author: state.users['1'],
+        comments: [
+          {
+            ...state.comments['1'],
+            author: state.users['1'],
+          },
+        ],
+      },
+    ]);
+
+    expect(getPostsAndCommentsWithUsers(state)).toBe(
+      getPostsAndCommentsWithUsers(state)
+    );
+
+    expect(getUsers).toHaveBeenCalledTimes(1);
+    expect(getPosts).toHaveBeenCalledTimes(1);
+    expect(getComments).toHaveBeenCalledTimes(1);
+
+    const nextState = { ...state };
+
+    expect(getPostsAndCommentsWithUsers(nextState)).toBe(
+      getPostsAndCommentsWithUsers(nextState)
+    );
+
+    expect(getUsers).toHaveBeenCalledTimes(2);
+    expect(getPosts).toHaveBeenCalledTimes(2);
+    expect(getComments).toHaveBeenCalledTimes(2);
   });
 });
--- src/createSelector.ts
+++ src/createSelector.ts
@@ -1,81 +1,286 @@
+/**
+ * Creates a memoized selector function with up to three inputs that are also
+ * memoized. The selector function (always the final argument) is only called
+ * when the inputs change.
+ *
+ * This is useful for creating functions that derive values from store state.
+ * It's designed to be used with the `useSelector()` hook that is created via
+ * `createStoreContext()`.
+ *
+ * Because the selector function passed to the `useSelector()` hook is called on
+ * every render and it causes the containing component to re-render when its
+ * return value changes, it's important to memoize the selector function if it
+ * is deriving a value from the store state.
+ *
+ * A selector function that is only used to extract a subset of the store state
+ * should not be memoized via this function. For example:
+ *
+ * ```tsx
+ * const Session = () => {
+ *   const currentUser = useSelector((state: State) => state.currentUser);
+ *
+ *   // ...
+ * }
+ * ```
+ *
+ * The selector above does not need to be memoized via `createSelector()`. In
+ * fact, passing it into `createSelector()` will only add unnecessary overhead
+ * since the result of the selector function is already "referentially stable"
+ * (i.e. it's not re-computed on each render).
+ *
+ * However, if the selector function is deriving a value from the store state,
+ * the result of the selector function may never be referentially stable if the
+ * value is non-primitive. For example:
+ *
+ * ```tsx
+ * const ActiveUsers = () => {
+ *   const activeUsers = useSelector((state: State) =>
+ *     state.users.filter(user => user.isActive)
+ *   );
+ *
+ *   // ...
+ * }
+ * ```
+ *
+ * The selector above will return a new array every time any part of the state
+ * changes. This will cause the `ActiveUsers` component to re-render every time
+ * the state changes, even if the array of users is the same as it was before
+ * the state change.
+ *
+ * We can improve this using `createSelector()`:
+ *
+ * ```tsx
+ * const getActiveUsers = createSelector(
+ *   (state: State) => state.users,
+ *   (users) => users.filter(user => user.isActive)
+ * );
+ *
+ * const ActiveUsers = () => {
+ *   const activeUsers = useSelector(getActiveUsers);
+ *
+ *   // ...
+ * }
+ * ```
+ *
+ * Now, the `ActiveUsers` component will only re-render when the array of users
+ * actually changes.
+ */
 export function createSelector<TState extends object, TResult>(
-  selector: (state: TState) => TResult
+  selector: (state: TState) => TResult,
+  options?: Options
 ): (state: TState) => TResult;
 
 export function createSelector<TState extends object, V1, TResult>(
   input1: (state: TState) => V1,
-  selector: (input1: V1) => TResult
+  selector: (input1: V1) => TResult,
+  options?: Options
 ): (state: TState) => TResult;
 
 export function createSelector<TState extends object, V1, V2, TResult>(
   input1: (state: TState) => V1,
   input2: (state: TState) => V2,
-  selector: (input1: V1, input2: V2) => TResult
+  selector: (input1: V1, input2: V2) => TResult,
+  options?: Options
 ): (state: TState) => TResult;
 
 export function createSelector<TState extends object, V1, V2, V3, TResult>(
   input1: (state: TState) => V1,
   input2: (state: TState) => V2,
   input3: (state: TState) => V3,
-  selector: (input1: V1, input2: V2, input3: V3) => TResult
+  selector: (input1: V1, input2: V2, input3: V3) => TResult,
+  options?: Options
 ): (state: TState) => TResult;
 
 export function createSelector<TState extends object, V1, V2, V3, TResult>(
   arg1: ((state: TState) => TResult) | ((state: TState) => V1),
-  arg2?: ((input1: V1) => TResult) | ((state: TState) => V2),
-  arg3?: ((input1: V1, input2: V2) => TResult) | ((state: TState) => V3),
-  arg4?: (input1: V1, input2: V2, input3: V3) => TResult
+  arg2?: ((input1: V1) => TResult) | ((state: TState) => V2) | Options,
+  arg3?:
+    | ((input1: V1, input2: V2) => TResult)
+    | ((state: TState) => V3)
+    | Options,
+  arg4?: ((input1: V1, input2: V2, input3: V3) => TResult) | Options,
+  arg5?: Options
 ) {
+  const v1Results = new WeakMap<TState, V1>();
+  const v2Results = new WeakMap<TState, V2>();
+  const v3Results = new WeakMap<TState, V3>();
+
   const results = new WeakMap<TState, TResult>();
 
-  if (arg4 !== undefined) {
+  if (typeof arg4 === 'function') {
     const input1 = arg1 as (state: TState) => V1;
     const input2 = arg2 as (state: TState) => V2;
     const input3 = arg3 as (state: TState) => V3;
     const selector = arg4;
+    const options = arg5;
 
     return (state: TState) => {
-      if (!results.has(state)) {
-        results.set(
-          state,
-          selector(input1(state), input2(state), input3(state))
-        );
+      if (results.has(state)) {
+        return results.get(state) as TResult;
+      }
+
+      let start: ReturnType<typeof performance.now> | undefined;
+
+      if (
+        process.env.NODE_ENV !== 'production' &&
+        typeof performance !== 'undefined' &&
+        options?.measure != null
+      ) {
+        start = performance.now();
+      }
+
+      if (!v1Results.has(state)) {
+        v1Results.set(state, input1(state));
+      }
+
+      if (!v2Results.has(state)) {
+        v2Results.set(state, input2(state));
+      }
+
+      if (!v3Results.has(state)) {
+        v3Results.set(state, input3(state));
+      }
+
+      results.set(
+        state,
+        selector(
+          v1Results.get(state) as V1,
+          v2Results.get(state) as V2,
+          v3Results.get(state) as V3
+        )
+      );
+
+      if (
+        process.env.NODE_ENV !== 'production' &&
+        typeof performance !== 'undefined' &&
+        options?.measure != null
+      ) {
+        performance.measure(options.measure.name, {
+          start,
+        });
       }
 
       return results.get(state) as TResult;
     };
-  } else if (arg3 !== undefined) {
+  } else if (typeof arg3 === 'function') {
     const input1 = arg1 as (state: TState) => V1;
     const input2 = arg2 as (state: TState) => V2;
     const selector = arg3 as (input1: V1, input2: V2) => TResult;
+    const options = arg4;
 
     return (state: TState) => {
-      if (!results.has(state)) {
-        results.set(state, selector(input1(state), input2(state)));
+      if (results.has(state)) {
+        return results.get(state) as TResult;
+      }
+
+      let start: ReturnType<typeof performance.now> | undefined;
+
+      if (
+        process.env.NODE_ENV !== 'production' &&
+        typeof performance !== 'undefined' &&
+        options?.measure != null
+      ) {
+        start = performance.now();
+      }
+
+      if (!v1Results.has(state)) {
+        v1Results.set(state, input1(state));
+      }
+
+      if (!v2Results.has(state)) {
+        v2Results.set(state, input2(state));
+      }
+
+      results.set(
+        state,
+        selector(v1Results.get(state) as V1, v2Results.get(state) as V2)
+      );
+
+      if (
+        process.env.NODE_ENV !== 'production' &&
+        typeof performance !== 'undefined' &&
+        options?.measure != null
+      ) {
+        performance.measure(options.measure.name, {
+          start,
+        });
       }
 
       return results.get(state) as TResult;
     };
-  } else if (arg2 !== undefined) {
+  } else if (typeof arg2 === 'function') {
     const input1 = arg1 as (state: TState) => V1;
     const selector = arg2 as (input1: V1) => TResult;
+    const options = arg3;
 
     return (state: TState) => {
-      if (!results.has(state)) {
-        results.set(state, selector(input1(state)));
+      if (results.has(state)) {
+        return results.get(state) as TResult;
       }
+
+      let start: ReturnType<typeof performance.now> | undefined;
+
+      if (
+        process.env.NODE_ENV !== 'production' &&
+        typeof performance !== 'undefined' &&
+        options?.measure != null
+      ) {
+        start = performance.now();
+      }
+
+      if (!v1Results.has(state)) {
+        v1Results.set(state, input1(state));
+      }
+
+      if (
+        process.env.NODE_ENV !== 'production' &&
+        typeof performance !== 'undefined' &&
+        options?.measure != null
+      ) {
+        performance.measure(options.measure.name, {
+          start,
+        });
+      }
+
+      results.set(state, selector(v1Results.get(state) as V1));
 
       return results.get(state) as TResult;
     };
   } else {
     const selector = arg1 as (state: TState) => TResult;
+    const options = arg2;
 
     return (state: TState) => {
       if (!results.has(state)) {
+        let start: ReturnType<typeof performance.now> | undefined;
+
+        if (
+          process.env.NODE_ENV !== 'production' &&
+          typeof performance !== 'undefined' &&
+          options?.measure != null
+        ) {
+          start = performance.now();
+        }
         results.set(state, selector(state));
+
+        if (
+          process.env.NODE_ENV !== 'production' &&
+          typeof performance !== 'undefined' &&
+          options?.measure != null
+        ) {
+          performance.measure(options.measure.name, {
+            start,
+          });
+        }
       }
 
       return results.get(state) as TResult;
     };
   }
+}
+
+interface Options {
+  measure?: {
+    name: string;
+  };
 }
