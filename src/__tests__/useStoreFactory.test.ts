a3916dee3499f531fafe88f752808e010331dab5
Dan Martens <dan@friendsoftheweb.com>
2025-05-23 11:23:28 -0400

    Upgrade dependencies (#21)
    * Upgrade dependencies
    
    * Upgrade Yarn

--- src/__tests__/useStoreFactory.test.ts
+++ src/__tests__/useStoreFactory.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 import { createStore } from '../createStore';
 import { useStoreFactory } from '../useStoreFactory';
 
